18182cbb969dd2bca546bf613b73528abfc9640b
nt-95 <xhorj@protonmail.com>
2021-12-08 16:24:39 +0100

    feat: started post html view

--- app/src/Views/Frontend/home.php
+++ app/src/Views/Frontend/home.php
@@ -1,4 +1,3 @@
-<h1>Un titre de home page</h1>
 
 <?php
 /**
@@ -6,8 +5,22 @@
  * @var $posts \App\Entity\Post[]
  */
 
-
-
-echo $test;
+ $post = $postManager->getPostById(1);
+var_dump($posts);
+var_dump($post->author);
 
 ?>
+
+<div class="home">
+    <div class="home__layout">
+        <div class="home__title">
+            <h1>Un titre de home page</h1>
+        </div>
+
+        <div class="container">
+            <h2>Blabla</h2>
+            <h2><?php "hello " . $post->getTitle(); ?></h2>
+        </div>
+    <div>
+
+</div>
