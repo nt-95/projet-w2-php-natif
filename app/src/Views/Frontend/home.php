7c8895f6aae343032eb6090e836c3d8c12352d92
nt-95 <xhorj@protonmail.com>
2021-12-08 16:54:36 +0100

    feat: display all posts in database in home.php

--- app/src/Views/Frontend/home.php
+++ app/src/Views/Frontend/home.php
@@ -1,26 +1,26 @@
-
 <?php
 /**
  * @var $user \App\Entity\Author
  * @var $posts \App\Entity\Post[]
  */
 
- $post = $postManager->getPostById(2);
-var_dump($posts);
-
 ?>
-
+ 
 <div class="home">
     <div class="home__layout">
         <div class="home__title">
-            <h1><?php echo "Welcome to MySpace " . $user; ?></h1>
+            <h1><?= "Welcome to MySpace " . $user; ?></h1>
         </div>
 
-        <div class="container">
-            <h2><?php echo $post->getTitle(); ?></h2>
-            <h3><?php echo "Posted by " . $post->getAuthor() . " on " . $post->getDate(); ?></h3>
-            <p><?php echo $post->getContent(); ?></p>
-        </div>
-    <div>
-
+        <?php
+        foreach ($posts as $post) :
+            ?>
+            <div class="container">
+                <h2><?= $post->getTitle(); ?></h2>
+                <h3><?= "Posted by " . $post->getAuthor() . " on " . $post->getDate(); ?></h3>
+                <p><?= $post->getContent(); ?></p>
+            </div>    
+        <?php endforeach; ?>
+        
+    <div>    
 </div>
