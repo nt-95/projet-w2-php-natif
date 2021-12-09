dfc67e45bb014ac92729c156feb08945d32afaa3
nt-95 <xhorj@protonmail.com>
2021-12-09 14:08:11 +0100

    feat: starting post method on creation form

--- app/src/Views/Frontend/create.php
+++ app/src/Views/Frontend/create.php
@@ -7,10 +7,29 @@
 ?>
 
 <h1>Write a new post</h1>
-<form>
+<form action='/' method='POST'>
     <label for="title">Title</label><br>
     <input type="text" id="title" name="title"><br>
     <label for="content">Content</label><br>
     <textarea name="content" cols="40" rows="5"></textarea><br>
      <input type="submit" value="Post">
 </form>
+
+<?php
+if(isset($_POST['submit'])){
+        echo "Yayyyy";
+    // Fetching variables of the form which travels in URL
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    if($title !=''&& $content !='')
+    {
+    //  To redirect form on a particular page
+    header("Location:localhost:5555/");
+    }
+    else{
+    ?>  <span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
+    }
+}
+else echo "pffff";
+// $postManager->createPost("hello")
+?>
