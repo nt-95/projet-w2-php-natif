033c0cf0895b45be4360ab821ad82030d2799bb2
nt-95 <xhorj@protonmail.com>
2021-12-09 15:49:09 +0100

    feat: the form can post data to the database

--- app/src/Views/Frontend/create.php
+++ app/src/Views/Frontend/create.php
@@ -1,35 +1,42 @@
 <?php
+
 /**
  * @var $user \App\Entity\Author
  * @var $posts \App\Entity\Post[]
  */
 
-?>
+use App\Fram\Utils\Flash;
 
-<h1>Write a new post</h1>
-<form action='/' method='POST'>
-    <label for="title">Title</label><br>
-    <input type="text" id="title" name="title"><br>
-    <label for="content">Content</label><br>
-    <textarea name="content" cols="40" rows="5"></textarea><br>
-     <input type="submit" value="Post">
-</form>
 
-<?php
-if(isset($_POST['submit'])){
-        echo "Yayyyy";
+ if(isset($_POST['submit'])){
     // Fetching variables of the form which travels in URL
     $title = $_POST['title'];
     $content = $_POST['content'];
     if($title !=''&& $content !='')
     {
-    //  To redirect form on a particular page
-    header("Location:localhost:5555/");
+        //  To redirect form on a particular page
+        // header("Location:/");
+        $post = $postManager->createPost($title, $content, $user);
+        if ($post) {
+            Flash::setFlash('success', 'Your post has been successfully created.');            
+        }
+        if(!$post) {
+            Flash::setFlash('alert', 'The post could not be created.');
+        }
     }
     else{
-    ?>  <span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
+    ?>  <span><?php Flash::setFlash('alert', 'Please fill in all the fileds.');?></span> <?php
     }
 }
-else echo "pffff";
-// $postManager->createPost("hello")
+
+
 ?>
+
+<h1>Write a new post</h1>
+<form method='POST'>
+    <label for="title">Title</label><br>
+    <input type="text" id="title" name="title"><br>
+    <label for="content">Content</label><br>
+    <textarea name="content" cols="40" rows="5"></textarea><br>
+     <input type="submit" name="submit" value="Post">
+</form>
