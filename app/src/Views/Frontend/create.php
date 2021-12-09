8df29f8156b25fed5b7d434181707cb03f1b1036
nt-95 <xhorj@protonmail.com>
2021-12-09 11:07:36 +0100

    feat: added post creation form

--- app/src/Views/Frontend/create.php
+++ app/src/Views/Frontend/create.php
@@ -6,4 +6,11 @@
 
 ?>
 
-<h1>This is the creation form</h1>
+<h1>Write a new post</h1>
+<form>
+    <label for="title">Title</label><br>
+    <input type="text" id="title" name="title"><br>
+    <label for="content">Content</label><br>
+    <textarea name="content" cols="40" rows="5"></textarea><br>
+     <input type="submit" value="Post">
+</form>
