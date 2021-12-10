a7ecc485023a94179a213a368ddde88c899e05a1
nt-95 <xhorj@protonmail.com>
2021-12-10 16:35:12 +0100

    feat: show picture in post (broken :'/ )

--- app/src/Views/Frontend/show.php
+++ app/src/Views/Frontend/show.php
@@ -1,9 +1,12 @@
+<?= $upload_dir . $post->getImageName();?>
+
+
 <div class="container border my-3 d-flex justify-content-center">
     <div class="container">
         <h1><?= $post_id; ?></h1>
         <h2 class="mt-2"><?= $post->getTitle(); ?></h2>
         <div><?= "Posted by " . $post->getAuthor() . " on " . $post->getDate(); ?></div>
-        <img class="img-fluid my-4" src="https://i.imgur.com/Eukht.jpeg"/>
+        <img class="img-fluid my-4" src=<?= $upload_dir . $post->getImageName();?>/>
         <p><?= $post->getContent(); ?></p>       
         <form class="mb-4" action="/remove/<?= $post_id?>" method="POST">
             <!-- <button class="btn btn-primary">Edit</button> -->
